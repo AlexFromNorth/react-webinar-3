7d615a1f91bf97ebd167441b66c638090700b96c
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2023-11-26 16:22:27 +0300

    fixed bug with "раз/раза"

--- src/ListEl.js
+++ src/ListEl.js
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
 
 const ListEl = ({ item, store }) => {
-  const [active, setActive] = useState(0);
+  const [numberActive, setNumberActive] = useState(0);
+
+  const resultCountNumber = counterNumber(numberActive)
+
+  function counterNumber(number) {
+    const lastDigit = number % 10
+    const lastTwoDigits = number % 100
+  
+    if(lastTwoDigits >= 11 && lastTwoDigits < 19) {
+      return number + ' раз'
+    }
+    if(lastDigit >= 2 && lastDigit <= 4) {
+      return number + ' раза'
+    } else {
+      return number + ' раз'
+    }
+  }
+
 
   return (
     <div
       className="List-item"
-      onClick={() => (item.selected ? setActive(active + 1) : false)}
+      onClick={() => (item.selected ? setNumberActive(numberActive + 1) : false)}
     >
       <div
         className={"Item" + (item.selected ? " Item_selected" : "")}
@@ -14,8 +31,8 @@ const ListEl = ({ item, store }) => {
       >
         <div className="Item-code">{item.code}</div>
         <div className="Item-title">
-          {item.title} {active > 0 ? 'Выделяли ' + active + " раз" : false}
-        </div>
+          {item.title} {numberActive > 0 ? 'Выделяли ' + resultCountNumber : false}
+        </div>-
         <div className="Item-actions">
           <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
         </div>
